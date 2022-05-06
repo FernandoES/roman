06fde774b3aba14d750a26a75a82753eeec144e6
espana.garcia <espana.garcia@netzwerkplan.de>
2022-05-06 19:12:00 +0200

    Add conversion from arabic to roman

---
+++ app/src/main/java/roman/RomanNumberConverter.java
@@ -0,0 +1,64 @@
+package roman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RomanNumberConverter {
+    private final String[] pseudoCodeRoman = {"","s","ss","sss","sf","f","fs","fss","fsss","st"};
+    private final List<Map<String, String>> pseudToRomanRelation = new ArrayList<Map<String, String>>();
+    
+    public RomanNumberConverter() {
+        initPseudoRomanRelation();
+    }
+
+    public List<String> convertArabicToPseudoRoman(int arabic) {
+        IntStream  streamOfChars = Integer.toString(arabic).chars();
+        return streamOfChars.mapToObj(num -> (pseudoCodeRoman[Character.getNumericValue(num)])).collect(Collectors.toList());
+    }
+
+    public String convertPseudoRomanToRoman(List<String> pseudo) {
+        return IntStream.range(0, pseudo.size()).mapToObj(index -> (converPseudoRomanSingleDigit(pseudo.get(index), pseudo.size() - index -1))).collect(Collectors.joining(""));
+    }
+
+    public String converPseudoRomanSingleDigit(String pseudoDigit, int position) {
+        return pseudoDigit.chars().mapToObj(letter -> pseudToRomanRelation.get(position).get(Character.toString(letter))).collect(Collectors.joining(""));
+
+    }
+
+    private void initPseudoRomanRelation() {
+        pseudToRomanRelation.add( new HashMap<String, String>() {{
+            put("s", "I");
+            put("f", "V");
+            put("t", "X");
+        }});
+        pseudToRomanRelation.add( new HashMap<String, String>() {{
+            put("s", "X");
+            put("f", "L");
+            put("t", "C");
+        }});
+        pseudToRomanRelation.add( new HashMap<String, String>() {{
+            put("s", "C");
+            put("f", "D");
+            put("t", "M");
+        }});
+        pseudToRomanRelation.add( new HashMap<String, String>() {{
+            put("s", "|I");
+            put("f", "|V");
+            put("t", "|X");
+        }});
+        pseudToRomanRelation.add( new HashMap<String, String>() {{
+            put("s", "|X");
+            put("f", "|L");
+            put("t", "|C");
+        }});
+        pseudToRomanRelation.add( new HashMap<String, String>() {{
+            put("s", "|C");
+            put("f", "|D");
+            put("t", "|M");
+        }});
+    }
+}
