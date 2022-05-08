331a36b9c2b6697e02c1704d5b19218f7b5e37f3
espana.garcia <espana.garcia@netzwerkplan.de>
2022-05-08 19:31:53 +0200

    Add a class to store error warnings

---
+++ app/src/main/java/roman/Errors.java
@@ -0,0 +1,8 @@
+package roman;
+
+public class Errors {
+    public static String unknownCharacterError = "Unknown character, the accepted characters in roman numbers are I,V,X,L,C,D,M;i,v,x,l,c,d,m";
+    public static String smallerThanOneNotAllowed = "Zero an negative numbers are not conceived in roman system, please introduce a positive number";
+    public static String tenMillionOrBigger = "Ten million and bigger numbers are noc conceived in roman system, the biggest accepted number is 9999999";
+    
+}
